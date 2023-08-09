1aae33d05499974b33ea3c1344d218c95cf9c0f6
butarvds <butarevds@mail.ru>
2023-08-09 22:15:53 +0700

    Creating Homework Subtasks 3.0, the 1+2 task

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,40 @@
 public class Main {
     public static void main(String[] args) {
         task1(); //variable Task 1 описание переменных
-        task2(); //Метод Task 2
+        task2(); //initializing variables Task 2 инициализация переменнных
     }
     public static void task1() {
         System.out.println(" Задача 1");
-        byte a = 127; // Переменная в пределах от -128 до 127
-        short b = 32767; // Переменная в пределах от -32768 до 32767
+        byte a = 127; // variable limit -128 до 127
+        System.out.println(" Переменная byte a = " +a);
+        short b = 32767; // variable limit -32768 до 32767
+        System.out.println(" Переменная short b = " +b);
+        int c = 2147483647; // variable limit -2147483648 до 2147483647
+        System.out.println(" Переменная int c = " +c);
+        long d = 922337203685477580L; // variable limit - many до many
+        System.out.println(" Переменная long d = " +d);
+        Float i = 1.152674748373f; //variable fractional
+        System.out.println(" Переменная Float i = " +i);
+        double f = 1.152674748373;
+        System.out.println(" Переменная double f = " +f);
     }
     public static void task2() {
+        System.out.println("__________________________________________________________________");
+        System.out.println(" Задача 2 инициализация переменных " + " 27.12, " + " 987 678 965 549, " + " 2.786, " + " 569, " + " -159, " + " 27897, " + " 67, ");
+        byte a = 67; // variable limit -128 до 127
+        System.out.println(" Переменная byte a = " +a);
+        short b1 = 569; // variable limit -32768 до 32767
+        System.out.println(" Переменная short b1 = " +b1);
+        short b2 = -159; // variable limit -32768 до 32767
+        System.out.println(" Переменная short b2 = " +b2);
+        short b3 = 27897; // variable limit -32768 до 32767
+        System.out.println(" Переменная short b3 = " +b3);
+        long d = 987678965549L; // variable limit - many до many
+        System.out.println(" Переменная long d = " +d);
+        Float i1 = 27.12f; //variable fractional
+        System.out.println(" Переменная Float i1 = " +i1);
+        Float i2 = 2.786f; //variable fractional
+        System.out.println(" Переменная Float i2 = " +i2);
 
-        System.out.println(" Задача 2");
     }
 }
