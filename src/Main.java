02395ba28d4e871698e3bd6cbc6bf869112b32d9
butarvds <butarevds@mail.ru>
2023-08-09 18:06:03 +0700

    Creating Homework Subtasks 3.0

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        task1(); //variable Task 1 описание переменных
+        task2(); //Метод Task 2
+    }
+    public static void task1() {
+        System.out.println(" Задача 1");
+        byte a = 127; // Переменная в пределах от -128 до 127
+        short b = 32767; // Переменная в пределах от -32768 до 32767
+    }
+    public static void task2() {
+
+        System.out.println(" Задача 2");
+    }
+}
