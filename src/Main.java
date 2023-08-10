2cb23159cfa407123ec1ebf4f9a8055c1fc5ec30
butarvds <butarevds@mail.ru>
2023-08-10 21:25:08 +0700

    Creating Homework Subtasks 3.0, the 1+2+3+4+5 task

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,8 @@ public class Main {
         task2(); //initializing variables Task 2 инициализация переменнных
         task3(); //three teachers Task 3 три учителя
         task4(); //bottle cutting machine Task 4 станок для бутылок
+        task5(); //paint for school Task 4 краска для школы
+
 
     }
     public static void task1() {
@@ -71,5 +73,25 @@ public class Main {
         System.out.println( " За месяц станок произвел = " +machineProductivityPerMonth+ " шт");
 
     }
+    public static void task5() {
+        System.out.println("___________________________________________________________________");
+        System.out.println(" Задача 4 краска для школы");
+        byte totalPaint = 120;
+        System.out.println(" Всего краски = " +totalPaint+ " банок");
+        byte  paintsOneClass= 2+4;// Proportion 2 of white 4 to brown
+        System.out.println(" Количество краски на один класс  = " +paintsOneClass+ " банок");
+        int  numberСlasses= totalPaint/paintsOneClass;
+        System.out.println(" Количество колическтво классов  = " +numberСlasses+ " аудиторий");
+        int  amountWhitePaint= numberСlasses*2;
+        System.out.println(" Общее количество белой краски  = " +amountWhitePaint+ " банок");
+        int  amountBrownPaint= numberСlasses*4;
+        System.out.println(" Общее количество белой краски  = " +amountBrownPaint+ " банок");
+        System.out.println(" В школе,где " +numberСlasses+ " , классов нужно " +amountWhitePaint+ " банок белой краски и " +amountBrownPaint+ " банок коричневой краски.");
+
+
+
+
+    }
+
 
 }
